8122fb2c52955cf8823c606b0be6d5d5d0ef4d77
Léo Peltier <contact@leo-peltier.fr>
2014-10-18 17:20:53 +0200

    Properly set headers.

--- public/index.php
+++ public/index.php
@@ -8,9 +8,9 @@ error_reporting(-1);
 require_once 'vendor/autoload.php';
 
 $app = new \Slim\Slim(['debug' => true]);
-
-$app->get('/', function()  {
-    phpinfo();
+$app->hook('slim.after.router', function () use ($app) {
+    if(strpos($app->request->getResourceUri(), '/api/') === 0)
+        $app->response->headers->set('Content-Type', 'application/json');
 });
 
 $app->group('/api', function() use ($app) {
@@ -26,4 +26,3 @@ $app->group('/api', function() use ($app) {
 });
 
 $app->run();
-header('Content-Type: application/json');
