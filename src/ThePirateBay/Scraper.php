a405822eaf57be7719cf3db45f79f8bb4bc5e2ca
Léo Peltier <contact@leo-peltier.fr>
2014-10-18 22:56:56 +0200

    Fix parsing of pages without image.

--- src/ThePirateBay/Scraper.php
+++ src/ThePirateBay/Scraper.php
@@ -11,10 +11,13 @@ class Scraper
     const CAT_VIDEO = 200;
     const ORDER_SEEDERS_DESC = 7;
 
-    const XPATH_TORRENT_LINK = "id('searchResult')/tr[1]/td[2]/div/a";
-    const XPATH_MAGNET       = "id('details')/div[4]/a[1]";
-    const XPATH_IMDB         = "id('details')/dl[1]/dd[4]/a";
-    const XPATH_HASH         = "id('details')/dl[1]";
+    const XPATH_TORRENT_LINK  = "id('searchResult')/tr[1]/td[2]/div/a";
+    const XPATH_MAGNET        = "id('details')/div[4]/a[1]";
+    const XPATH_MAGNET_W_IMG  = "id('details')/div[4]/a[1]";
+    const XPATH_MAGNET_WO_IMG = "id('details')/div[3]/a[1]";
+    const XPATH_IMDB          = "id('details')/dl[1]/dd[4]/a";
+    const XPATH_HASH_W_IMG    = "id('details')/dl[1]";
+    const XPATH_HASH_WO_IMG   = "id('details')/dl[2]";
 
     /**
      * @param string $query search string.
@@ -46,8 +49,8 @@ class Scraper
     protected static function getTorrentImdb($xml)
     {
         $imdb = $xml->xpath(self::XPATH_IMDB);
-        if(count($imdb) !== 1)
-            throw new \RuntimeException("No imdb for torrent at `$url`.");
+        if (count($imdb) !== 1)
+            throw new \RuntimeException('No imdb for torrent.');
         sscanf((string) $imdb[0]['href'], "http://www.imdb.com/title/%s", $url);
         return trim($url, '/');
     }
@@ -58,9 +61,12 @@ class Scraper
      */
     protected static function getTorrentHash($xml)
     {
-        $hash = $xml->xpath(self::XPATH_HASH);
-        if(count($hash) !== 1)
-            throw new \RuntimeException("No hash for torrent at `$url`.");
+        $hash = $xml->xpath(self::XPATH_HASH_W_IMG);
+        if (empty($hash[0]) || strlen(trim($hash[0])) <= 0)
+            $hash = $xml->xpath(self::XPATH_HASH_WO_IMG);
+
+        if (empty($hash[0]) || strlen(trim($hash[0])) <= 0)
+            throw new \RuntimeException('No hash for torrent.');
 
         return strtoupper(trim((string) $hash[0]));
     }
@@ -71,9 +77,14 @@ class Scraper
      */
     protected static function getTorrentMagnet($xml)
     {
-        $magnet = $xml->xpath(self::XPATH_MAGNET);
-        if(count($magnet) !== 1)
-            throw new \RuntimeException("No magnet for torrent at `$url`.");
+        $magnet = $xml->xpath(self::XPATH_MAGNET_W_IMG);
+
+        if (empty($magnet[0]['href']) || strpos((string) $magnet[0]['href'], 'magnet:') !== 0)
+            $magnet = $xml->xpath(self::XPATH_MAGNET_WO_IMG);
+
+        if (empty($magnet[0]['href']) || strpos((string) $magnet[0]['href'], 'magnet:') !== 0)
+            throw new \RuntimeException('No magnet for torrent.');
+
         return (string) $magnet[0]['href'];
     }
 
@@ -89,7 +100,7 @@ class Scraper
         );
         $xml = self::xmlFromUrl($url);
         $links = $xml->xpath(self::XPATH_TORRENT_LINK);
-        if(count($links) !== 1)
+        if (count($links) !== 1)
             throw new \RuntimeException("No result for query `$query`.");
 
         return self::BASE_URL . ((string) $links[0]['href']);
@@ -104,7 +115,7 @@ class Scraper
         // TODO: dirty cache, make it less dirty.
         $cache = '/tmp/marie.html.' . md5($url);
 
-        if(file_exists($cache)) {
+        if (file_exists($cache)) {
             $raw = file_get_contents($cache);
         } else {
             $raw = file_get_contents($url);
