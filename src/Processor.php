462acee34a3556839f3f6092b3da9a0e6b9d488f
Léo Peltier <contact@leo-peltier.fr>
2014-10-19 04:04:32 +0200

    Bootstrap process.

--- src/Processor.php
+++ src/Processor.php
@@ -8,23 +8,61 @@ use Duchesse\Chaton\Marie\Models\Movie;
 class Processor
 {
     protected $em;
+    protected $api;
     protected $imdbId;
+    protected $videosDir;
+    protected $sourcesDir;
 
     public function __construct($imdbId)
     {
         $this->em = Util::getEntityManager();
+        $this->api = Util::getTransmissionApi();
         $this->imdbId = $imdbId;
+        $this->videosDir  = dirname(__DIR__) . '/videos';
+        $this->sourcesDir = $this->videosDir . '/sources';
+
+        if (!file_exists($this->videosDir))
+            mkdir($this->videosDir, 0755, true);
+        if (!file_exists($this->sourcesDir))
+            mkdir($this->sourcesDir, 0755, true);
     }
 
     public function __invoke()
     {
-        syslog(LOG_INFO, "Starting processing movie #{$this->imdbId}.");
+        syslog(LOG_INFO, "Start processing movie #{$this->imdbId}.");
 
-        for($i = 0; $i <= 100; $i += 10) {
-            echo $i . PHP_EOL;
-            sleep(1);
-        }
+        $movie = $this->em->getRepository('Marie:Movie')->find($this->imdbId);
+        if ($movie === null)
+            throw new \RuntimeException('Movie not found.');
+
+        $realPath = $this->getMovieRealpath($movie);
+        $sourcePath = realpath($this->sourcesDir) . '/' . $movie->torrentHash;
+        link($realPath, $sourcePath);
+
+        $gif = new \GifTool(
+            basename($sourcePath),
+            '/opt/ffmpeg/ffprobe',
+            $this->videosDir . '/',
+            $this->sourcesDir . '/',
+            '/opt/ffmpeg/ffmpeg'
+        );
 
         syslog(LOG_INFO, "Done processing movie #{$this->imdbId}.");
+    }
+
+    protected function getMovieRealpath(Movie $movie)
+    {
+        $torrent = $this->api->get($movie->torrentHash);
+        if (!$torrent->isFinished())
+            throw new \RuntimeException('Torrent not finished.');
+
+        $biggest = $torrent->getFiles()[0];
+        foreach ($torrent->getFiles() as $file) {
+            if ($file->getSize() > $biggest->getSize())
+                $biggest = $file;
+        }
+        assert('$biggest->getCompleted() === $biggest->getSize()');
+
+        return realpath($this->api->getSession()->getDownloadDir() . '/' . $biggest->getName());
     }
 }
