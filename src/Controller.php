9a947f2e5a3437b7c7e0b27bb504c7572d1d6e27
Léo Peltier <contact@leo-peltier.fr>
2014-10-19 10:29:12 +0200

    Reduce quality.

--- src/Controller.php
+++ src/Controller.php
@@ -178,14 +178,14 @@ class Controller
         if (!file_exists($out)) {
             $cmd = Util::strTpl(
                 '/opt/ffmpeg/ffmpeg -i {in} -ss {start} -t {duration}'
-                . ' -codec:v libvpx -quality good -cpu-used 0 -b:v 500k -qmin 10'
-                . ' -qmax 42 -maxrate 500k -bufsize 1000k -threads 4'
+                . ' -codec:v libvpx -quality good -cpu-used 0 -b:v 300k -qmin 10'
+                . ' -qmax 42 -maxrate 300k -bufsize 1000k -threads 5 -an'
                 . ' -vf scale=-1:480 {out}',
                 [
-                    'in' => escapeshellarg($in),
-                    'out' => escapeshellarg($out),
-                    'start' => escapeshellarg($start),
-                    'duration' => escapeshellarg($stop - $start),
+                   'in'       => escapeshellarg($in),
+                   'out'      => escapeshellarg($out),
+                   'start'    => escapeshellarg($start),
+                   'duration' => escapeshellarg($stop    - $start),
                 ]
             );
             exec($cmd, $_, $exit);
