383e9a739257a868614cb984509e234c1ec2e19e
Léo Peltier <contact@leo-peltier.fr>
2014-10-19 06:41:15 +0200

    Fix route order.

--- index.php
+++ index.php
@@ -31,8 +31,8 @@ $app->get('/', function() {
 $app->group('/api', function() use ($app) {
     $controller = new Controller($app);
     $app->get('/movie/list',              [$controller, 'movieList']);
-    $app->get('/movie/:imdbId(/:create)', [$controller, 'movieGet']);
     $app->get('/movie/stream/:imdbId',    [$controller, 'movieGetStream']);
+    $app->get('/movie/:imdbId(/:create)', [$controller, 'movieGet']);
     $app->get('/torrent/search/:query',   [$controller, 'torrentSearch']);
     $app->get('/status',                  [$controller, 'status']);
 });
